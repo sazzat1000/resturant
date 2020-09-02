0a90edd6f60e140d86ed4a8b78aba29d4abedf85
Sazzat <62603676+sazzat1000@users.noreply.github.com>
2020-09-02 21:15:41 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // Smooth Scroll
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  // Sticky Menu
+  $("#menu").waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  });
+  new WOW().init();
+
+});
